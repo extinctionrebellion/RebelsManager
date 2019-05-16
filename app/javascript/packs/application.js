a46a98fee2398de6af8881f47e4582a55634c4c0
Michael Hulet <michael@hulet.eu>
2019-05-16 16:49:03 +0200

    Set up webpacks with Foundation and Stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import $ from 'jquery'
+window.$ = $
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import 'controllers'
+import 'foundation-sites'
+
+window.App = {}
+
+App.init = () =>
+  $(document).foundation()
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  App.init()
+});

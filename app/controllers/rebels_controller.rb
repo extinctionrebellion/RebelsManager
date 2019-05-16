4a28350d44bf991fc7cdb59afcccf38103a4762e
Michael Hulet <michael@hulet.eu>
2019-05-16 17:10:03 +0200

    Rebels#new form

--- app/controllers/rebels_controller.rb
+++ app/controllers/rebels_controller.rb
@@ -1,2 +1,27 @@
 class RebelsController < ApplicationController
+  def new
+    @rebel = Rebel.new
+  end
+
+  def create
+    @rebel = Rebel.new(rebel_params)
+    if @rebel.save
+      redirect_to new_rebel_path(
+        notice: "Merci, nous te recontactons prochainement!"
+      )
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rebel_params
+    params.require(:rebel).permit(
+      :name,
+      :email,
+      :phone,
+      :notes
+    )
+  end
 end

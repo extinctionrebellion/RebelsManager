42b781a9521d091b0a5019d3ce5640a889e87acb
Michael Hulet <michael@hulet.eu>
2019-06-14 06:29:59 +0200

    Enable Devise timeoutable for users (10 minutes)

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable,
-         :lockable, :trackable
+         :lockable, :trackable, :timeoutable
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Whitelist locales available for the application
-I18n.available_locales = [:fr]
+I18n.available_locales = [:en]
 
 # Set default locale to something other than :en
-I18n.default_locale = :fr
+I18n.default_locale = :en

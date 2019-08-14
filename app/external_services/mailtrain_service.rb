771e40ec75b81d0fa764968e32e6057ab25e18cb
Michael Hulet <michael@hulet.eu>
2019-08-14 10:46:31 +0200

    Add the MailtrainService class with subscribe method

---
+++ app/external_services/mailtrain_service.rb
@@ -0,0 +1,26 @@
+class MailtrainService
+  include Singleton
+
+  # --- API docs ---
+  # see https://lists.extinctionrebellion.be/account/api
+
+  def subscribe(list_id, params = {})
+    url = "#{ENV['MAILTRAIN_API_ENDPOINT']}/subscribe/#{list_id}?access_token=#{ENV['MAILTRAIN_API_TOKEN']}"
+    RestClient::Request.execute(
+      method:     :post,
+      url:        url,
+      payload:    params,
+      headers:    {
+                    accept: :json,
+                    params: params
+                  }
+    ) do |response, request, result, block|
+      case response.code
+      when 200
+        JSON.parse(response)
+      else
+        response.return!(request, result, &block)
+      end
+    end
+  end
+end

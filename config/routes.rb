526579511ef986e1474400e0793007f33c301e6e
Michael Hulet <michael@hulet.eu>
2019-08-21 11:33:22 +0200

    Remove symbol image and redirect /manager to /rebels

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root to: 'rebels#index'
 
+  get '/manager', to: redirect('/rebels')
+  get '/manager/rebels', to: redirect('/rebels')
+
   devise_for :users,
              path: 'auth',
              path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', unlock: 'unlock' }

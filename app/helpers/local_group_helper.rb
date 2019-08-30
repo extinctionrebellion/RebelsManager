bc25a2d39cffe944966abe390ab75ad1e28c3f84
Michael Hulet <michael@hulet.eu>
2019-08-31 00:10:01 +0200

    Code cleanup

--- app/helpers/local_group_helper.rb
+++ app/helpers/local_group_helper.rb
@@ -1,7 +1,11 @@
 module LocalGroupHelper
-    def delete_link(local_group, current_user)
-        if current_user.admin && local_group.destroyable?
-            link_to("Delete", local_group_path(local_group), method: :delete, data: { confirm: 'Are you sure?' }, class: 'secondary button')
-        end
+  def delete_link(local_group, current_user)
+    if current_user.admin? && local_group.destroyable?
+      link_to "Delete",
+              local_group_path(local_group),
+              method: :delete,
+              data: { confirm: 'Are you sure?' },
+              class: 'secondary button'
     end
+  end
 end

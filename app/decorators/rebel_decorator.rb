089212b92cc9829b1ffd5deaa6f974278e027109
Michael Hulet <michael@hulet.eu>
2019-10-10 06:34:35 +0200

    Localize public strings and provide FR translations

--- app/decorators/rebel_decorator.rb
+++ app/decorators/rebel_decorator.rb
@@ -2,13 +2,13 @@ class RebelDecorator < DecoratorBase
   def availability
     case target.availability
     when "unavailable"
-      _("Currently unavailable")
+      "Currently unavailable"
     when "few_hours_a_month"
-      _("A few hours a month")
+      "A few hours a month"
     when "few_hours_a_week"
-      _("A couple of hours a week")
+      "A couple of hours a week"
     when "10_hours_a_week"
-      _("More than 10 hours a week")
+      "More than 10 hours a week"
     when nil
       "-"
     end

42c8170ca6c8023b127c9209101951d8dabfd8ab
Agathe Begault <agathe.begault@gmail.com>
2019-10-20 18:47:42 +0200

    Remove email column from rebels table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_164439) do
+ActiveRecord::Schema.define(version: 2019_10_20_164551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_164439) do
 
   create_table "rebels", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

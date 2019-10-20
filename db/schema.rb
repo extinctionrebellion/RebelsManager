560954c0d3b0e21a8c3f42e552746e854b0d063a
Agathe Begault <agathe.begault@gmail.com>
2019-10-20 18:45:40 +0200

    Remove phone columns from rebels table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_155822) do
+ActiveRecord::Schema.define(version: 2019_10_20_164439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_155822) do
   create_table "rebels", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "phone"
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

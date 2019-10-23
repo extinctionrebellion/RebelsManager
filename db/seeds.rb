e86bef69837f36f99f7f1d19a75902926c612f08
Michael Hulet <michael@hulet.eu>
2019-10-23 08:15:34 +0200

    Add multiple local groups and coordinators accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,51 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 if User.none?
-  User.create(email: 'admin@example.com', password: 'admin123', admin: true)
+  # Admin user account
+  User.create(email: 'admin@organise.earth', password: 'admin123', admin: true)
 end
 
 if Rebel.none?
-  local_group = LocalGroup.find_or_create_by(name: FFaker::Address.city)
+  # Local groups
+  local_group = LocalGroup.find_or_create_by(name: FFaker::Address.unique.city)
+  local_group2 = LocalGroup.find_or_create_by(name: FFaker::Address.unique.city)
 
+  # Local group coordinators user accounts
+  User.create(
+    email: 'local-group@organise.earth',
+    password: 'admin123',
+    local_group: local_group
+  )
+  User.create(
+    email: 'local-group2@organise.earth',
+    password: 'admin123',
+    local_group: local_group2
+  )
+
+  # Working groups for both local groups
   WorkingGroup.create(local_group: local_group, name: 'Action & Strategy', color: '#16A938')
   WorkingGroup.create(local_group: local_group, name: 'Communication & IT', color: '#3760AA')
   WorkingGroup.create(local_group: local_group, name: 'Outreach', color: '#985C9B')
+  WorkingGroup.create(local_group: local_group2, name: 'Action & Strategy', color: '#16A938')
+  WorkingGroup.create(local_group: local_group2, name: 'Communication & IT', color: '#3760AA')
+  WorkingGroup.create(local_group: local_group2, name: 'Outreach', color: '#985C9B')
 
-  tags = ['', 'safe-list', 'xr-cafe', 'local-group-coordinator']
+  # Meaningful tags
+  tags = ['', 'safe-list', 'xr-cafe', 'coordinator']
 
+  # Rebel statuses
+  statuses = ['active', 'paused', 'inactive']
+
+  # Create rebels
   20.times do
     rebel = Rebel.create(
       name: FFaker::Name.unique.name,
       email: FFaker::Internet.unique.email,
       consent: true,
       language: 'en',
-      local_group: local_group,
+      status: statuses.sample,
       tag_list: tags.sample,
+      local_group: LocalGroup.order("RANDOM()").take,
       working_groups: WorkingGroup.limit(1).order("RANDOM()")
     )
   end

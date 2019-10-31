1d1da4fc766d9e6e157a6692fc943555eaf33d72
Agathe <agathe.begault@gmail.com>
2019-10-31 15:24:56 +0100

    [Security][Encrypting] Remove legacy fields (3/5) (#94)
    * Add lockbox to gemfile
    
    * Add blind_index to gemfile
    
    * Add configuration file
    
    * Add encryption columns for user email
    
    * Add encryption columns for rebels email
    
    * Add encryption columns for rebels phone
    
    * Add migration options to user email and rebel phone/email fields
    
    * Upgrade new migrations version
    
    * Ignore legacy fields (email and phone) after encryption setup
    
    * Remove phone columns from rebels table
    
    * Remove email column from rebels table
    
    * Remove email column from users table
    
    * Remove :migrating option
    
    * Remove ignored_columns line

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable,
          :lockable, :trackable, :timeoutable
 
-  encrypts :email, migrating: true
-  blind_index :email, migrating: true
+  encrypts :email
+  blind_index :email
 
   belongs_to :local_group, optional: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_155822) do
+ActiveRecord::Schema.define(version: 2019_10_23_052405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,8 +65,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_155822) do
 
   create_table "rebels", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "phone"
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -133,7 +131,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_155822) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -152,7 +149,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_155822) do
     t.boolean "admin"
     t.string "email_ciphertext"
     t.string "email_bidx"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["email_bidx"], name: "index_users_on_email_bidx", unique: true
     t.index ["local_group_id"], name: "index_users_on_local_group_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

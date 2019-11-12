a1637895d4c82d568592dce66886e8b3a2af893b
Michael Hulet <michael@hulet.eu>
2019-11-12 07:11:30 +0100

    Use jobs for adding and deleting Mailtrain subscriptions

---
+++ app/jobs/mailtrain/add_subscriptions_job.rb
@@ -0,0 +1,48 @@
+class Mailtrain::AddSubscriptionJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(rebel)
+    subscribe_to_rebels_list(rebel)
+    subscribe_to_rebels_local_list(rebel)
+  end
+
+  private
+
+  def subscribe_to_rebels_list(rebel)
+    MailtrainService.instance.add_subscription(
+      ENV['MAILTRAIN_REBELS_LIST_ID'],
+      {
+        "EMAIL": rebel.email,
+        "MERGE_NAME": rebel.name,
+        "MERGE_LOCAL_GROUP": rebel.local_group&.name,
+        "MERGE_LANGUAGE": rebel.language,
+        "MERGE_POSTCODE": rebel.postcode,
+        "MERGE_PROFILE_URL": rebel.profile_url,
+        "MERGE_TAGS": rebel.tag_list,
+        "MERGE_SKILLS": rebel.skills&.pluck(:name)&.join("|"),
+        "MERGE_WORKING_GROUPS": rebel.working_groups&.pluck(:name)&.join("|"),
+        "FORCE_SUBSCRIBE": "yes",
+        "TIMEZONE": ENV['XR_BRANCH_TIMEZONE']
+      }
+    )
+  end
+
+  def subscribe_to_rebels_local_list(rebel)
+    return if rebel.local_group&.mailtrain_list_id.nil?
+    MailtrainService.instance.add_subscription(
+      rebel.local_group.mailtrain_list_id,
+      {
+        "EMAIL": rebel.email,
+        "MERGE_NAME": rebel.name,
+        "MERGE_STATUS": rebel.status,
+        "MERGE_POSTCODE": rebel.postcode,
+        "MERGE_PROFILE_URL": rebel.profile_url,
+        "MERGE_TAGS": rebel.tag_list,
+        "MERGE_SKILLS": rebel.skills&.pluck(:name)&.join("|"),
+        "MERGE_WORKING_GROUPS": rebel.working_groups&.pluck(:name)&.join("|"),
+        "FORCE_SUBSCRIBE": "yes",
+        "TIMEZONE": ENV['XR_BRANCH_TIMEZONE']
+      }
+    )
+  end
+end
---
+++ app/jobs/mailtrain/delete_subscriptions_job.rb
@@ -0,0 +1,30 @@
+class Mailtrain::DeleteSubscriptionJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(email, local_group_id)
+    unsubscribe_from_rebels_list(email)
+    unsubscribe_from_rebels_local_list(email, local_group_id)
+  end
+
+  private
+
+  def unsubscribe_from_rebels_list(email)
+    MailtrainService.instance.delete_subscription(
+      ENV['MAILTRAIN_REBELS_LIST_ID'],
+      {
+        "EMAIL": email
+      }
+    )
+  end
+
+  def unsubscribe_from_rebels_local_list(email, local_group_id)
+    local_group = LocalGroup.find_by(id: local_group_id)
+    return if local_group&.mailtrain_list_id.nil?
+    MailtrainService.instance.delete_subscription(
+      local_group.mailtrain_list_id,
+      {
+        "EMAIL": email
+      }
+    )
+  end
+end

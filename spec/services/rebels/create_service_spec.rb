db7b89b75e349626033d2a378333dac9d4b161d8
Michael Hulet <michael@hulet.eu>
2019-11-22 10:56:38 +0100

    Add first specs

--- spec/services/rebels/create_service_spec.rb
+++ spec/services/rebels/create_service_spec.rb
@@ -2,16 +2,35 @@ require "rails_helper"
 
 RSpec.describe Rebels::CreateService, "#run" do
   describe "When email format is not valid" do
-    params = {
-      rebel: {
-        email: "badly_formatted_email@provider",
-        language: "en"
-      }
-    }
+    it "doesn't create the rebel" do
+      params = ActionController::Parameters.new({
+        rebel: {
+          email: "badly_formatted_email@provider",
+          language: "en"
+        }
+      })
 
-    service = Rebels::CreateService.new(source: "admin")
-    service.run(params)
+      service = Rebels::CreateService.new(source: "admin")
+      service.run(params)
 
-    expect(service.error_message).to eq("Please double check the email address provided.")
+      expect(Rebel.count).to eq(0)
+    end
+  end
+
+  describe "When the rebel has been created" do
+    it "has a token" do
+      params = ActionController::Parameters.new({
+        rebel: {
+          email: FFaker::Internet.safe_email,
+          language: "en"
+        }
+      })
+
+      service = Rebels::CreateService.new(source: "admin")
+      service.run(params)
+
+      rebel = Rebel.first
+      expect(rebel.token).to be_truthy
+    end
   end
 end
